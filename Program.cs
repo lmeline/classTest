4a966b44a8d8f9d46eefec9394807c24df6f1ef8
loise meline <loise13@hotmail.fr>
2024-12-19 16:16:05 +0100

    verifmdp

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿
-
-// Créer un objet utilisateur de la classe Utilisateur 
+﻿// Créer un objet utilisateur de la classe Utilisateur 
 // déclaration d'un entier 
     // int i = 0;
 
 // déclaration d'une variable de type Utilisateur
-Utilisateur utilisateur = new Utilisateur("Meline","Loise","coco@lolo.fr","lolololo");
+Utilisateur utilisateur = new Utilisateur("Meline","Loise","coco@lolo.fr","lolololo1");
 
+// afficher les trucs dans la console 
 Console.WriteLine(utilisateur.Prenom);
 // créer la classe 
 class Utilisateur {
@@ -15,28 +14,61 @@ class Utilisateur {
 public string? Nom { get; set; }
 public string? Prenom { get; set; }
 public string? Email { get; set; }
-public string? MotDePasse { get; set; }
-private string mdp;
-public string Mdp {get{return mdp;}set{mdp= value; }}
+//public string? MotDePasse { get; set; }
+private string? mdp;
+public string? Mdp {
+    get{
+        return mdp;
+    }
+    set{
+        if(verifMdp (value)){
+            mdp = value;
+            Console.WriteLine("bravo champion");
+        } else {
+            Console.WriteLine("Non");
+        }
+
+    }
+}
 
 // reviens au même, mais plus complexe 
     // private string email;
     // public string Email { get { return email;} set { email = value; } }
 
-// créer un constructeur 
-    public Utilisateur(string nom, string prenom, string email, string motDePasse) {
+
+    // créer un constructeur (méthode)
+    public Utilisateur(string nom, string prenom, string email, string mdp) {
+   
         Nom = nom;
         Prenom = prenom;
         Email = email;
-        MotDePasse = motDePasse;
+        Mdp = mdp;
         
     }
 
+    // créer une méthode 
+    private bool verifMdp(string? mdp){
 
+        int nb1 = 0;
+        int nb2 = 0;
+
+       foreach (char c in mdp) {
+        if (Char.IsDigit(c))
+        {
+            nb1++;
+        }
+        else if (Char.IsLetter(c)) {
+            nb2++;
+        }
+        
+       }
+       if (nb1 >= 1 && nb2 >= 1){
+        return true;
+       } else {
+        return false;
+       }
+    
+    }
 }
 
-
-class verifMdp {
-
-}
 
--- obj/Debug/net8.0/classTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/classTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6af88c6dd353410cc3e9421563b0b8de5fff30")]
 [assembly: System.Reflection.AssemblyProductAttribute("classTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
